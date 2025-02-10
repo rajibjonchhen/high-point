e6df491c9aa917e95865b8613cbc405a6874941b
rajibjonchhen <rajib.jonchhen@gmail.com>
2025-02-10 16:20:31 +0000

    some update in the content and images

--- vite.config.ts
+++ vite.config.ts
@@ -52,41 +52,11 @@ export default defineConfig({
                                     avatar: getAvatar("oneflow"),
                                 },
                                 images: [
-                                    { type: "image/png", src: "/src/img/display/ladies.png" },
+                                    { type: "image/png", src: "/src/img/display/all.png" },
                                     { type: "image/webp", src: "/src/img/display/Imageoneflow.webp" },
                                 ],
                                 logo: getLogo("oneflow")
 
-                            },
-                            {
-                                title: "Gents’ Attire",
-                                text: "Discover our range of stylish and comfortable clothing for men. From smart casuals to rugged outdoor wear, our gents' collection is designed to keep you looking sharp no matter the occasion. Find everything from shirts and trousers to jackets and accessories.",
-                                quote: "Skilled engineers and responsible managers. They’ve been our trusted partners throughout the years and I am happy to work with them for another decade.",
-                                author: {
-                                    name: "Nigel Watson",
-                                    position: "Business Incubation Director",
-                                    avatar: getAvatar("hp"),
-                                },
-                                images: [
-                                    { type: "image/png", src: "/src/img/display/gents.png" },
-                                    { type: "image/webp", src: "/src/img/display/Imagehp.webp" },
-                                ],
-                                logo: getLogo("hp")
-                            },
-                            {
-                                title: "Kids’ Clothing",
-                                text: "Dress your little ones in style with our adorable kids’ clothing line. We offer a wide variety of outfits for boys and girls, including playful everyday wear and special occasion attire. Our clothes are designed with comfort and durability in mind, perfect for active kids!",
-                                quote: "Brilliant developers, you’ll hardly find any better. Happy to work with them any day. In fact, I am already working with them on my next startup.",
-                                author: {
-                                    name: "Ben Zhuk",
-                                    position: "Co-founder, Head of Product",
-                                    avatar: getAvatar("pronoun"),
-                                },
-                                images: [
-                                    { type: "image/png", src: "/src/img/display/kids.png" },
-                                    { type: "image/webp", src: "/src/img/display/Imagepronoun.webp" },
-                                ],
-                                logo: getLogo("pronoun")
                             },
                             {
                                 title: "All-Season Styles",
